b68e097040dc0838df98ed47807ae68eb629dd8b
Jonas Hübotter <me@jonhue.me>
2018-03-06 16:33:32 +0100

    Update tabs_generator.rb

--- lib/generators/myg/tabs_generator.rb
+++ lib/generators/myg/tabs_generator.rb
@@ -14,7 +14,7 @@ class Myg::TabsGenerator < Rails::Generators::Base
     def create_templates
         template '_tabs.html.erb', "app/views/#{options[:controller]}/_tabs.html.erb"
         options[:method_names]&.each do |name|
-            @name = name
+            @name = name[:name].underscore
             template 'tab.html.erb', "app/views/#{options[:controller]}/_#{@name}.html.erb"
             template '_tab.html.erb', "app/views/#{options[:controller]}/#{@name}.html.erb"
         end
