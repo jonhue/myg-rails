f3df69e158a9b3ba2f91574edb1fcda7d0c883d4
Jonas Hübotter <me@jonhue.me>
2018-04-16 13:13:05 +0200

    Update railtie.rb

--- lib/myg/railtie.rb
+++ lib/myg/railtie.rb
@@ -17,6 +17,7 @@ module Myg
                 config.define_component 'myg/slider', discrete: false, disabled: false, value: 0, min: 0, max: 50, label: 'Select value'
                 config.define_component 'myg/switch'
                 config.define_component 'myg/tabs', async: false
+                config.define_component 'myg/textarea'
                 config.define_component 'myg/titlebar', close: true
                 config.define_component 'myg/toast'
                 config.define_component 'myg/toolbar'
